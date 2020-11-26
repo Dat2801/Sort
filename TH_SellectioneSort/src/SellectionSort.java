c60b7382bfd892a7704ece67690bfea0161b85ff
Dat Bong <Datbong2801@>
2020-11-26 08:52:36 +0700

    new

---
+++ TH_SellectioneSort/src/SellectionSort.java
@@ -0,0 +1,30 @@
+public class SellectionSort {
+
+    static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+
+    public static void selectionSort(double[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            double currentMin = list[i];
+            int temp = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    temp = j;
+                }
+            }
+
+            if (temp != i) {
+                list[temp] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] + " ");
+        }
+    }
+}
